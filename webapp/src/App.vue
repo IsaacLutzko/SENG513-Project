e004562e0f3b98b499a11c1d5d2b0e3b98a25721
ahad.hamirani <ahad.hamirani@cs.ucalgary.ca>
2022-04-09 01:25:21 -0600

    Added hamburger navbar functionality

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -8,6 +8,178 @@
           /></router-link>
         </div>
 
+        <div
+          class="offcanvas offcanvas-start"
+          tabindex="-1"
+          id="offcanvasExample"
+          aria-labelledby="offcanvasExampleLabel"
+        >
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+            <button
+              type="button"
+              class="btn-close text-reset"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="offcanvas-body">
+            <div class="nav-text-list">
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login' &&
+                    this.$route.path != '/hiringmanager-explore' &&
+                    this.$route.path != '/hiringmanager-matches' &&
+                    this.$route.path != '/hiringmanager-postedjobs' &&
+                    this.$route.path != '/hiringmanager-addjob'
+                  "
+                  to="/jobseeker-explore"
+                  class="nav-item"
+                  >Explore</router-link
+                >
+              </div>
+
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login' &&
+                    this.$route.path != '/hiringmanager-explore' &&
+                    this.$route.path != '/hiringmanager-matches' &&
+                    this.$route.path != '/hiringmanager-postedjobs' &&
+                    this.$route.path != '/hiringmanager-addjob'
+                  "
+                  to="/jobseeker-matches"
+                  class="nav-item"
+                  >Matches</router-link
+                >
+              </div>
+
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login' &&
+                    this.$route.path != '/hiringmanager-explore' &&
+                    this.$route.path != '/hiringmanager-matches' &&
+                    this.$route.path != '/hiringmanager-postedjobs' &&
+                    this.$route.path != '/hiringmanager-addjob'
+                  "
+                  to="#"
+                  class="nav-item"
+                  >Profile</router-link
+                >
+              </div>
+
+              <!-- HiringManager -->
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login' &&
+                    this.$route.path != '/jobseeker-explore' &&
+                    this.$route.path != '/jobseeker-matches' &&
+                    this.$route.path != '/jobseeker-profile' &&
+                    this.$route.path != '/jobseeker-edit-profile'
+                  "
+                  to="/hiringmanager-explore"
+                  class="nav-item"
+                  >Explore</router-link
+                >
+              </div>
+
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login' &&
+                    this.$route.path != '/jobseeker-explore' &&
+                    this.$route.path != '/jobseeker-matches' &&
+                    this.$route.path != '/jobseeker-profile' &&
+                    this.$route.path != '/jobseeker-edit-profile'
+                  "
+                  to="/hiringmanager-matches"
+                  class="nav-item"
+                  >Matches</router-link
+                >
+              </div>
+
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login' &&
+                    this.$route.path != '/jobseeker-explore' &&
+                    this.$route.path != '/jobseeker-matches' &&
+                    this.$route.path != '/jobseeker-profile' &&
+                    this.$route.path != '/jobseeker-edit-profile'
+                  "
+                  to="/hiringmanager-postedjobs"
+                  class="nav-item"
+                  >Jobs</router-link
+                >
+              </div>
+
+              <!-- Universal logout -->
+              <div
+                class="text-reset"
+                aria-label="Close"
+                data-bs-dismiss="offcanvas"
+              >
+                <router-link
+                  v-if="
+                    this.$route.path != '/' &&
+                    this.$route.path != '/register' &&
+                    this.$route.path != '/createaccount' &&
+                    this.$route.path != '/login'
+                  "
+                  to="/login"
+                  class="nav-item"
+                  >Logout</router-link
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div class="full-size">
           <!-- Job seeker -->
           <div class="nav-text-container">
@@ -117,7 +289,27 @@
             >
           </div>
         </div>
-        <div class="collapsed-button"></div>
+        <div
+          v-if="
+            this.$route.path != '/' &&
+            this.$route.path != '/register' &&
+            this.$route.path != '/createaccount' &&
+            this.$route.path != '/login'
+          "
+          class="collapsed-button"
+        >
+          <a
+            class="btn"
+            data-bs-toggle="offcanvas"
+            href="#offcanvasExample"
+            role="button"
+            aria-controls="offcanvasExample"
+          >
+            <div class="burger"></div>
+            <div class="burger"></div>
+            <div class="burger"></div>
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -183,6 +375,12 @@ export default {
   align-items: center;
 }
 
+.nav-text-list {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
 .page-container {
   padding-bottom: 25px;
 }
@@ -200,10 +398,35 @@ export default {
   font-weight: bolder;
 }
 
+.collapsed-button {
+  display: none;
+}
+
+.burger {
+  width: 35px;
+  height: 5px;
+  background-color: black;
+  margin: 6px 0;
+}
+
 .nav-item:hover {
   background-image: linear-gradient(90deg, #fc8801, #dea26a);
   background-clip: text;
   -webkit-text-fill-color: transparent;
+}
+
+@media only screen and (max-width: 700px) {
+  .full-size {
+    display: none;
+  }
+
+  .collapsed-button {
+    display: flex;
+    height: 5vh;
+    position: absolute;
+    right: 5%;
+    top: 3%;
+  }
 }
 
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
